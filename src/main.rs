02209c8d2ef89db5afa029714cc2f40cdd0a7590
b8p0b <se77enn@gmail.com>
2018-11-22 23:05:31 -0800

    fetch function accepts equity choice from user through CLI. Catch if no arg is provided. Does not handle if equity is invalid as API response is still 200

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,8 @@ extern crate serde_json;
 use serde_json::Value;
 use reqwest::{Client, Response};
 use std::str::FromStr;
+use std::env;
+
 // use std::collections::LinkedList;
 // use std::vec::Vec;
 use serde::export::Vec;
@@ -20,15 +22,20 @@ fn get_most_recent_price(prices: &mut Vec<f32>) -> f32 {
     return prices[length - 1]; // get earliest price stock 
 }
 // use std::collections::HashMap
-fn fetch() -> std::result::Result<Vec<f32>, reqwest::Error> {
+fn fetch(equity: &str) -> std::result::Result<Vec<f32>, reqwest::Error> {
     let client = Client::new();
-
+    let mut own_string: String = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=".to_owned();
+    let bor_string: &str = equity;
+    own_string.push_str(bor_string);
+    let bor_string2: &str = "&interval=1min&apikey=IVO96IWUXGF22KP9";
+    own_string.push_str(bor_string2); 
+    println!("{}", own_string);
     let json = |mut res : Response | {
         res.json::<Value>()
     };
     let request1 =
         client
-            .get("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=GOOGL&interval=1min&apikey=IVO96IWUXGF22KP9")
+            .get(&own_string)
             .send()
             .and_then(json);
          request1.map(|res1|{
@@ -59,7 +66,14 @@ fn fetch() -> std::result::Result<Vec<f32>, reqwest::Error> {
 }
 
 fn main() {
- let x = fetch();
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("Please provide the equity of your choosing. Need 1 arg");
+        return;
+    }
+    let query = &args[1];
+    println!("{:?}", query);
+ let x = fetch(query);
 match x {
     Ok(v) => println!("{:?}", v.len()),
     Err(e) => println!("error parsing header: {:?}", e),
